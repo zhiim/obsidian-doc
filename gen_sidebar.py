c9f242ec6da01620096f6bc39ef7c604971e775f
Q Xu <xuq3196@outlook.com>
2026-05-05 19:40:42 +0800

    do not add a line after "返回上一级"

--- gen_sidebar.py
+++ gen_sidebar.py
@@ -103,7 +103,6 @@ def generate_files_for_current_dir(root, dirs, files):
     sidebar_lines = []
     if back_link:
         sidebar_lines.append(f"* [🔙 返回上一级]({back_link})")
-        sidebar_lines.append("")
 
     for name, url in folder_links:
         sidebar_lines.append(f"* [**{name}**]({url})")
